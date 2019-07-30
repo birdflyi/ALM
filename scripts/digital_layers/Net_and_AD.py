06b40c27d17e1a24cc5dd7a78c74cf85b5ee4509
娄泽华 <louzehua@thunisoft.com>
2019-07-30 15:20:15 +0800

    Add: Add files ommited in the last commit(SHA-1:2fe1db36c8069c9aa8f5bb5317802d124c5be212).

---
+++ scripts/digital_layers/Net_and_AD.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python 3.6
+import os
+
+import torch.nn as nn
+
+from etc import filePathConf, extensions
+
+from scripts.feature_fitting_layers.Net_and import Net_and
+from scripts.digital_layers.Net_step import Net_step
+from scripts.structure.Net_template import Net_template
+
+__author__ = 'ALM'
+__time__ = '2019-07-30 15:02:07.303666'
+
+class Net_and_AD(Net_template):
+    def __init__(self, in_features=2, out_features=1, class_alias=None):
+        super(Net_and_AD, self).__init__(in_features, out_features, class_alias)
+        self.net_sequence = nn.Sequential(
+            Net_and(in_features=2, out_features=1),
+            Net_step(in_features=1, out_features=1)
+        )
+        self.set_caller_pyfile_path(os.path.abspath(__file__))
+        self.check_purpose()
+        self.summary()
+
+
+if __name__ == '__main__':
+    net = Net_and_AD()
+    # save and load model pairs
+    save_dir = filePathConf.absPathDict[filePathConf.MODELS_STATE_DICT_DIR]
+    ext_state_dict = extensions.ext_models[extensions.EXT_MODELS__STATE_DICT]
+    net.reset_save_model_path(save_dir=save_dir, save_mode=extensions.EXT_MODELS__STATE_DICT)
+    model = net.load_state_dict_model(net._save_model_path)
+    model.set_caller_pyfile_path(os.path.abspath(__file__))
+    model.check_purpose()
+    model.summary()
+    model.save_whole_model()
+    model.save_state_dict_model()
+    # print messages of net
+    print(model.class_alias)
+    print(model.__dict__)
